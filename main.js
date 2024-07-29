ee971b41782804507dae10fa50a6a8bbd04db1dc
viktorKorneev <korneevviktor90@yandex.ru>
2024-07-29 20:35:29 +0300

    step-5

--- main.js
+++ main.js
@@ -6,24 +6,35 @@ let buildingName = "Tower";
 let costGold = 30;
 
 // проверяем, хватает ли нам золота для стрительсва
-if (gold >= costGold) {
-  // вычитаем золото
-  gold -= costGold; // то же, что gold = gold - costGold
-  console.log(`${buildingName}: work complete!`);
-} else {
-  console.log(`${buildingName}: not enough resources!`);
-}
+// if (gold >= costGold) {
+//   // вычитаем золото
+//   gold -= costGold; // то же, что gold = gold - costGold
+//   console.log(`${buildingName}: work complete!`);
+// } else {
+//   console.log(`${buildingName}: not enough resources!`);
+// }
 
 // Попытка построить кузницу
- buildingName = "Blacksmith";
- costGold = 140;
-
-
+buildingName = "Blacksmith";
+costGold = 140;
 
 function createBulding(buildingName, costGold) {
-    if (gold >= costGold) {
-        gold -= costGold / console.log(`${buildingName}: work complete!`);
-      } else {
-        console.log(`${buildingName}: not enough resources!`);
-      }
+  const hasEnoughGold = gold >= costGold;
+
+  if (hasEnoughGold) {
+    gold -= costGold;
+    console.log(`${buildingName}: work complete!`);
+  } else {
+    console.log(`${buildingName}: not enough resources!`);
+  }
 }
+createBulding("Tower", 30); // Строим башню золота хватает
+createBulding("Blacksmith", 140); // Строить кузницу, золота не хватает
+
+// Функция для увеличения ресурсов
+function increaseResource(currentAmount, increment) {
+    return currentAmount + increment
+}
+
+gold = increaseResource(gold, 50)
+console.log(`New amount of gold: ${gold}`);
