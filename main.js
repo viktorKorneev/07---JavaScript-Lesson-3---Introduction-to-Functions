3e9bcc20846042ad84189fdf667bed942c840b99
viktorKorneev <korneevviktor90@yandex.ru>
2024-07-29 20:16:07 +0300

    step-2

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 // Объявляем глобальную переменную для хранения золота
-let gold = 100
+let gold = 100;
 
 // Попытка построить бошню
-let buildingName = "Tower"
-let costGold = 30
+let buildingName = "Tower";
+let costGold = 30;
 
 // проверяем, хватает ли нам золота для стрительсва
 if (gold >= costGold) {
-    // вычитаем золото
-    gold -= costGold // то же, что gold = gold - costGold
-    console.log(`${buildingName}: work complete!`);
+  // вычитаем золото
+  gold -= costGold; // то же, что gold = gold - costGold
+  console.log(`${buildingName}: work complete!`);
 } else {
-    console.log(`${buildingName}: not enough resources!`);
+  console.log(`${buildingName}: not enough resources!`);
+}
+
+// Попытка построить кузницу
+ buildingName = "Blacksmith";
+ costGold = 140;
+
+if (gold >= costGold) {
+  gold -= costGold / console.log(`${buildingName}: work complete!`);
+} else {
+  console.log(`${buildingName}: not enough resources!`);
 }
